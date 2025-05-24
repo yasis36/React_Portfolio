99937175abe396a985b157bd298d96cb4a332322
Sisay <116642894+yasis36@users.noreply.github.com>
2025-05-24 17:37:51 +0300

    Update Gallery.js

--- src/component/Gallery.js
+++ src/component/Gallery.js
@@ -1,34 +1,65 @@
 import React, { useState } from "react";
 // import "./Gallery.css"; // Import CSS for styling
-import image1 from "../img/Software Develpment.jpg";
-import image2 from "../img/data-management.webp";
-import image3 from "../img/Graphics_Design.jpeg";
-import image4 from "../img/ui-ux-.jpg";
-import image5 from "../img/AI.jpg";
-import image6 from "../img/sb-blog-programming.png";
-import image7 from "../img/video-editing.jpg";
-import image8 from "../img/Digital Marketing.jpg";
+import image1 from "../img/project/km1.png";
+import image3 from "../img/project/km3.png";
+import image4 from "../img/project/km4.png";
+import image5 from "../img/project/km5.png";
+import image6 from "../img/project/km6.png";
+import image7 from "../img/project/lp1.png";
+import image8 from "../img/project/lp2.png";
+import image9 from "../img/project/lp3.png";
+import image10 from "../img/project/lp4.png";
+import image11 from "../img/project/lp5.png";
+import image12 from "../img/project/lp6.png";
+import image13 from "../img/project/lw1.png";
+import image14 from "../img/project/lw2.png";
+import image15 from "../img/project/lw3.png";
+import image16 from "../img/project/lw4.png";
+import image17 from "../img/project/lw5.png";
+import image18 from "../img/project/lw6.png";
+
+
+
+
 
 const Gallery = () => {
   const galleries = [
     {
       id: 1,
+      title: "Tchnology Solutions Portflio",
       mainImage: image1,
       images: [
         image1,
-        image2,
         image3,
         image4,
+        image5,
+        image6,
       ],
     },
     {
       id: 2,
-      mainImage: image5,
+      title: "Personal Portflio",
+      mainImage: image7,
       images: [
-        image5,
-        image6,
         image7,
         image8,
+        image9,
+        image10,
+        image11,
+        image12,
+      ],
+    },
+     {
+      id: 3,
+      title: "Personal Portflio",
+      mainImage: image13,
+      images: [
+        image13,
+        image14,
+        image15,
+        image16,
+        image17,
+        image18,
       ],
     },
     
@@ -48,7 +79,7 @@ const Gallery = () => {
 
   return (
     <div className="gallery-container">
-      <h2> Gallery</h2>
+      <h2> Gallery of My Work </h2>
       <div className="gallery-grid">
         {galleries.map((gallery) => (
           <div key={gallery.id} className="gallery-item">
@@ -57,6 +88,8 @@ const Gallery = () => {
               alt={`Gallery ${gallery.id}`}
               onClick={() => openGallery(gallery)}
             />
+            <p className="gallery_title"> {gallery.title}</p>
+
           </div>
         ))}
       </div>
