18ffccf238da4f8affb102b8ae99b225d4488a01
Sisay <116642894+yasis36@users.noreply.github.com>
2026-04-02 11:51:02 +0300

    Update Skill.js

--- src/component/Skill.js
+++ src/component/Skill.js
@@ -1,59 +1,52 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faJs, faReact, faNodeJs, faCss3Alt, faHtml5, faPython, faJava, faCuttlefish,faWordpress, faFigma,faJoomla } from '@fortawesome/free-brands-svg-icons';
+import {
+  faJs,
+  faReact,
+  faNodeJs,
+  faCss3Alt,
+  faHtml5,
+  faPython,
+  faJava,
+  faWordpress,
+  faFigma,
+  faJoomla,
+} from '@fortawesome/free-brands-svg-icons';
+import { faDatabase, faPalette } from '@fortawesome/free-solid-svg-icons';
 
 const skills = [
-    { name: 'JavaScript', icon: faJs },
-    { name: 'React', icon: faReact },
-    { name: 'Node.js', icon: faNodeJs },
-    { name: 'CSS',  icon: faCss3Alt },
-    { name: 'HTML',  icon: faHtml5 },
-    { name: 'Python',  icon: faPython },
-    { name: 'Java',icon: faJava },
-    { name: 'C++',  icon: faCuttlefish },
-    { name: 'Wordpress',  icon:faWordpress},
-    { name: 'Figma',  icon:faFigma},
-    { name: 'Joomla',  icon:faJoomla},
+  { name: 'JavaScript', icon: faJs },
+  { name: 'React', icon: faReact },
+  { name: 'Node.js', icon: faNodeJs },
+  { name: 'CSS', icon: faCss3Alt },
+  { name: 'HTML', icon: faHtml5 },
+  { name: 'Python', icon: faPython },
+  { name: 'Java', icon: faJava },
+  { name: 'Database Design', icon: faDatabase },
+  { name: 'WordPress', icon: faWordpress },
+  { name: 'Figma', icon: faFigma },
+  { name: 'Joomla', icon: faJoomla },
+  { name: 'Brand Styling', icon: faPalette },
+];
 
+const Skill = () => {
+  return (
+    <div className="content-block skill-block">
+      <div className="section-heading reveal-up">
+        <p className="eyebrow">Skills</p>
+        <h2>Tools and capabilities I use to turn concepts into finished work</h2>
+      </div>
 
-    
-    ];
-
-
- const Skill = () => {
-    // const [currentSkillIndex] = useState(0);
-
-    // useEffect(() => {
-    //     const interval = setInterval(() => {
-    //         setCurrentSkillIndex((prevIndex) => (prevIndex + 1) % skills.length);
-    //     }, 3000); // Change skill every 2 seconds
-
-    //     return () => clearInterval(interval);
-    // }, []);
-
-    return (
-        
-        <div className="skill-container" >
-            <h2>My Skills</h2>
-            <div className="skill-slide">
-                {skills.map((skill, index) => (
-                    <div
-                        key={index}
-                        className='skill-item'
-                    >
-                        
-                        <div className="skill-name">
-                            <FontAwesomeIcon icon={skill.icon} className="skill-icon"/> {skill.name}
-                        </div>
-                      
-                        </div>
-                ))}
-            </div>
-                      
-        </div>
-        
-        
-    );
+      <div className="skills-grid">
+        {skills.map((skill) => (
+          <div key={skill.name} className="skill-item reveal-up">
+            <FontAwesomeIcon icon={skill.icon} className="skill-icon" />
+            <span>{skill.name}</span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Skill;
